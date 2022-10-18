4f2653d09f864a0848bab0b36009518f68c18936
Matheus Torres <matios.torrres@gmail.com>
2022-10-18 14:50:05 -0300

    removed some redundances in main code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import "./css/index.css"
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
-const ccBgColor03 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
-const ccBgColor04 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
 function setCardType(type) {
@@ -23,10 +21,10 @@ function setCardType(type) {
   }
   ccBgColor01.setAttribute("fill", colors[type][0])
   ccBgColor02.setAttribute("fill", colors[type][1])
-  ccBgColor03.setAttribute("fill", colors[type][2])
-  ccBgColor04.setAttribute("fill", colors[type][3])
+  ccBgColor02.setAttribute("fill", colors[type][2])
+  ccBgColor02.setAttribute("fill", colors[type][3])
 
   ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
-setCardType("visa")
+setCardType("elo")
