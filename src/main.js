5c69fda2f4b8d2fd5561ae07fb605c877fb64adc
Matheus Torres <matios.torrres@gmail.com>
2022-10-20 01:49:14 -0300

    added form clear funcionality

--- src/main.js
+++ src/main.js
@@ -102,9 +102,7 @@ addButton.addEventListener("click", () => {
   alert("Cartão Adicionado!")
 })
 
-document.querySelector("form").addEventListener("submit", (event) => {
-  event.preventDefault()
-})
+
 
 const cardHolder = document.querySelector("#card-holder")
 cardHolder.addEventListener("input", () => {
